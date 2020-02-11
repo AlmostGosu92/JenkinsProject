e14e3ad5725a645df13b9b7a2c7aecdcbf8eade0
Axel Eriksson <a.eriksson0303@gmail.com>
2020-02-11 10:29:22 +0100

    added Contact class with constructor

---
+++ src/contacts/Contact.java
@@ -0,0 +1,27 @@
+package contacts;
+
+public class Contact {
+	
+	String name, mail, number;
+	
+	public Contact(String name, String mail, String number) {
+		this.name = name;
+		this.mail = mail;
+		this.number = number;
+	}
+	
+//	*Kod för sökmetod
+//	for (Contact contact : ConList) {
+//		if (in.equals(contact.getName()){
+//			return contact;
+//		}
+//		else if (in.equals(contact.getMail)) {
+//			return contact;
+//		}
+//		else if (in.equals(contact.getNumber)) {
+//			return contact;
+//		}
+//	}
+//	return "Contact not found";
+
+}
