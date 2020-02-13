391265e54120c702f9d4c17e76d1357a7bb04877
Axel Eriksson <a.eriksson0303@gmail.com>
2020-02-13 11:41:52 +0100

    Set descriptive texts in interface

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -4,12 +4,13 @@ import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.Label;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 public interface UI {
-	public static void startGUI() {
+	private static void startGUI() {
 		JFrame frame = new JFrame("Test");
 		frame.setSize(500, 500);
 		frame.setVisible(true);
@@ -19,11 +20,13 @@ public interface UI {
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		
-		tabbedPane.addTab("Tab1", makePanel("This is tab 1"));
-		tabbedPane.addTab("Tab2", makePanel("This is tab 2"));
+		tabbedPane.addTab("Contactbook", makePanel("Contacts will go here"));
+		tabbedPane.addTab("Add Contact", makePanel("Here is where you will add contacts"));
 		
 		frame.getContentPane().add(tabbedPane);
-
+		
+		JButton addContactButton = new JButton("Add Contact");
+		
 	}
 
 	private static Component makePanel(String string) {
