546911b39dbd72cc44e618cf42e4f11965cadc94
Crunchwrap89 <marcus89n@gmail.com>
2020-02-14 10:33:05 +0100

    added code to classes

--- src/contacts/Contact.java
+++ src/contacts/Contact.java
@@ -9,19 +9,29 @@ public class Contact {
 		this.mail = mail;
 		this.number = number;
 	}
-	
-//	*Kod för sökmetod
-//	for (Contact contact : ConList) {
-//		if (in.equals(contact.getName()){
-//			return contact;
-//		}
-//		else if (in.equals(contact.getMail)) {
-//			return contact;
-//		}
-//		else if (in.equals(contact.getNumber)) {
-//			return contact;
-//		}
-//	}
-//	return "Contact not found";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
 
 }
--- src/contacts/ContactBookSearch.java
+++ src/contacts/ContactBookSearch.java
@@ -1,5 +1,23 @@
 package contacts;
 
-public class ContactBookSearch {
+public class ContactBookSearch  {
+	
+
+//	public void Search() {
+//		
+//		for (Contact contact : ConList) {
+//		if (in.equals(contact.getName()){
+//			return contact;
+//		}
+//		else if (in.equals(contact.getMail)) {
+//			return contact;
+//		}
+//		else if (in.equals(contact.getNumber)) {
+//			return contact;
+//		}
+//	}
+//	return "Contact not found";
+//		
+//	}
 
 }
