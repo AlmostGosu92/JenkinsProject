c45ea5014c3ed7b0527d37c9bcc29aa9a90f2d63
Anners <anners@mailzorz.com>
2020-02-14 10:46:22 +0100

    load och save metod

--- src/filesync/Filesync.java
+++ src/filesync/Filesync.java
@@ -1,5 +1,29 @@
 package filesync;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
 public class Filesync {
 
+	public void Load() throws FileNotFoundException {
+		File file = new File("kontaktlista.txt");
+		Scanner radraknare = new Scanner(file);
+		Scanner sc = new Scanner(file);
+		int antalRader = 0;
+//		räknar antalet rader i filen
+		while(radraknare.hasNextLine()) {
+			antalRader++;
+			radraknare.nextLine();
+		}
+//		läser av alla parametrar i constructorn per rad
+		for (int i = 0; i < antalRader; i++) {
+			namn.add(new Contacts(sc.next(), sc.next(), sc.next()));
+			sc.hasNextLine();
+
+		}
+
+	}
 }
