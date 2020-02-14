24d691b636303c966922d231829071aa01a62855
Crunchwrap89 <marcus89n@gmail.com>
2020-02-14 11:23:30 +0100

    added methods to ContactBook

--- src/contacts/ContactBook.java
+++ src/contacts/ContactBook.java
@@ -5,5 +5,20 @@ import java.util.ArrayList;
 public class ContactBook {
 	
 	public ArrayList<Contact> contacts = new ArrayList<>();
+	
+	
+//	public Contact removeContact(String name, String email, String number) {
+//		contacts.remove(input);
+// }	
 
-}
+//  public Contact editContact(String name, String email, String number) {
+//		contacts.remove(input);
+// }
+	
+//  public Contact addContact(String name, String email, String number) {
+//		contacts.add(input);
+//}
+	
+	
+	}
+
