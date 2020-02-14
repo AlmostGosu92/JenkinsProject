98956b6ff087dc615e96b8d92d6f3e08c1e3fc2a
Crunchwrap89 <marcus89n@gmail.com>
2020-02-14 12:00:42 +0100

    added code and removed editmethod from contactbook

--- src/contacts/ContactBook.java
+++ src/contacts/ContactBook.java
@@ -7,13 +7,9 @@ public class ContactBook {
 	public ArrayList<Contact> contacts = new ArrayList<>();
 	
 	
-//	public Contact removeContact(String name, String email, String number) {
-//		contacts.remove(input);
-// }	
-
-//  public Contact editContact(String name, String email, String number) {
-//		contacts.remove(input);
-// }
+	public void removeContact(String name, String lastName, String email, String number) {
+		contacts.remove(0);  // 0 skall ändras till den plats i indexet som markerats för borttag.
+ }	
 	
   public void addContact(String name, String lastName, String email, String number) {
 	  contacts.add(new Contact(name, lastName, email ,number));
