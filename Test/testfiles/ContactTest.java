b7e780676b9e16eb6ea838dc27f9facb043962bd
Crunchwrap89 <marcus89n@gmail.com>
2020-02-15 13:27:21 +0100

    added testcases to ContactTest

--- Test/testfiles/ContactTest.java
+++ Test/testfiles/ContactTest.java
@@ -1,17 +1,34 @@
 package testfiles;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
 import contacts.Contact;
+import contacts.ContactBook;
 
 class ContactTest {
 
 	@Test
-	void TestContactsAreNotTheSame() {
+	void TestContactsNameCorrect() {
 		Contact kontakt1 = new Contact("Lars", "Roffel", "larsr@gmail.com", "101010");
-		Contact kontakt2 = new Contact("Lars", "Ruffelito", "larsr@gmail.com", "101010");
-		assertNotEquals(kontakt1, kontakt2);
+		assertEquals(kontakt1.getName(), "Lars");
+}
+	@Test
+	void TestContactsNameNotCorrect() {
+		Contact kontakt1 = new Contact("Lars", "Roffel", "larsr@gmail.com", "101010");
+		assertNotEquals(kontakt1.getName(), "Klasse");
+}	
+	@Test
+	void TestContactsMailCorrect() {
+		Contact kontakt1 = new Contact("Lars", "Roffel", "larsr@gmail.com", "101010");
+		assertEquals(kontakt1.getMail(), "larsr@gmail.com");
+}
+	
+	@Test
+	void TestContactsMailNotCorrect() {
+		Contact kontakt1 = new Contact("Lars", "Roffel", "larsr@gmail.com", "101010");
+		assertNotEquals(kontakt1.getName(), "felmailadress@gmail.com");
 }
 }
