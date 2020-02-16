d0cb9004a7ebe51ac6dd83c8b7aafdd0c5851315
Anners <anners@mailzorz.com>
2020-02-16 19:14:55 +0100

    gjort klart filsyncTester och lagt till variabel för vilken fil man ska spara till

--- Test/testfiles/FileSyncTest.java
+++ Test/testfiles/FileSyncTest.java
@@ -1,14 +1,11 @@
 package testfiles;
 
-
-
-
-
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -17,37 +14,48 @@ import org.junit.jupiter.api.Test;
 import contacts.ContactBook;
 import filesync.Filesync;
 
-
-
 public class FileSyncTest {
 
 	Filesync f = new Filesync();
-	
 
-	
 	@Test
-	void LoadTest() {
+	void LoadTest() throws IOException {
+		ContactBook c = new ContactBook();
+
 		
+			f.setFilnamn("Test.txt");
+			BufferedWriter out = new BufferedWriter(new FileWriter(f.getFilnamn()));
+			out.write("Test Testsson Test@test.test 08-434343");
+			out.close();
+			f.Load();
+			assertEquals(c.contacts.get(0).getLastName(), "Testsson");
+			c.contacts.clear();
+		
+
 	}
-	
+
 	@Test
 	void SaveTest() throws IOException {
-		ContactBook c = new ContactBook();
-		c.addContact("Peter", "Petersson", "peter@hotmail.com", "070-9367431");
-		c.addContact("Eric", "Ericsson", "eric@hotmail.com", "08-4343435");
-		f.save();
-		int tokens = 0;
-		File file = new File("kontaktlista.txt");
-		Scanner scan = new Scanner(file);
-		while(scan.hasNext() == true) {
-			
-			tokens++;
-			scan.next();
+
 		
-		}
-		assertEquals(8, tokens);
+			ContactBook c = new ContactBook();
+			c.addContact("Peter", "Petersson", "peter@hotmail.com", "070-9367431");
+			c.addContact("Eric", "Ericsson", "eric@hotmail.com", "08-4343435");
+			f.setFilnamn("saveTest.txt");
+			f.save();
+			int tokens = 0;
+			File file = new File(f.getFilnamn());
+			Scanner scan = new Scanner(file);
+			while (scan.hasNext() == true) {
+
+				tokens++;
+				scan.next();
+
+			}
+
+			assertEquals(8, tokens);
+			c.contacts.clear();
 		
-		
-		
+
 	}
 }
--- src/filesync/Filesync.java
+++ src/filesync/Filesync.java
@@ -18,13 +18,24 @@ public class Filesync {
 
 	ContactBook b = ui.UI.cb;
 	
-	public void load() throws FileNotFoundException {
-		File file = new File("kontaktlista.txt");
+	private String filnamn = "kontaktlista.txt";
+
+	public String getFilnamn() {
+		return filnamn;
+	}
+
+	public void setFilnamn(String filnamn) {
+		this.filnamn = filnamn;
+	}
+	
+	public void Load() throws FileNotFoundException {
+		
+		File file = new File(filnamn);
 		Scanner radraknare = new Scanner(file);
 		Scanner sc = new Scanner(file);
 		int antalRader = 0;
 //		räknar antalet rader i filen
-		while(radraknare.hasNextLine()) {
+		while (radraknare.hasNextLine()) {
 			antalRader++;
 			radraknare.nextLine();
 		}
@@ -34,17 +45,23 @@ public class Filesync {
 			sc.hasNextLine();
 
 		}
+		
 
 	}
 	public void save() throws IOException {
-		BufferedWriter out = new BufferedWriter(new FileWriter("kontaktlista.txt"));
+		
+		BufferedWriter out = new BufferedWriter(new FileWriter(filnamn));
 		for (int i = 0; i < b.contacts.size(); i++) {
 			out.write(b.contacts.get(i).getName() + " ");
 			out.write(b.contacts.get(i).getLastName() + " ");
 			out.write(b.contacts.get(i).getMail() + " ");
 			out.write(b.contacts.get(i).getNumber() + " ");
 			out.newLine();
-	}
+
+		}
+
 		out.close();
+
+
 	}
 }
