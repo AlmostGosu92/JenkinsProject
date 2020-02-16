78769c2c2ae2f8c4636a3bad6ede33b92fd648f9
Axel Eriksson <a.eriksson0303@gmail.com>
2020-02-16 13:14:40 +0100

    changed .equals to .equalsIgnoreCase in search function

--- src/contacts/ContactBookSearch.java
+++ src/contacts/ContactBookSearch.java
@@ -8,17 +8,17 @@ public class ContactBookSearch  {
 	public String searchContact(String string) {
 		
 		for (Contact contact : UI.cb.contacts) {
-		if (string.equals(contact.getName())){
-			return contact.getName() + " " + contact.getLastName() + " " + contact.getMail() + " " + contact.getNumber();
+		if (string.equalsIgnoreCase(contact.getName())){
+			return contact.getName() + " " + contact.getLastName() + ", " + contact.getMail() + ", " + contact.getNumber();
 		}
-		else if (string.equals(contact.getLastName())) {
-			return contact.getName() + " " + contact.getLastName() + " " + contact.getMail() + " " + contact.getNumber();
+		else if (string.equalsIgnoreCase(contact.getLastName())) {
+			return contact.getName() + " " + contact.getLastName() + ", " + contact.getMail() + ", " + contact.getNumber();
 		}
-		else if (string.equals(contact.getMail())) {
-			return contact.getName() + " " + contact.getLastName() + " " + contact.getMail() + " " + contact.getNumber();
+		else if (string.equalsIgnoreCase(contact.getMail())) {
+			return contact.getName() + " " + contact.getLastName() + ", " + contact.getMail() + ", " + contact.getNumber();
 		}
-		else if (string.equals(contact.getNumber())) {
-			return contact.getName() + " " + contact.getLastName() + " " + contact.getMail() + " " + contact.getNumber();
+		else if (string.equalsIgnoreCase(contact.getNumber())) {
+			return contact.getName() + " " + contact.getLastName() + ", " + contact.getMail() + ", " + contact.getNumber();
 		}
 	}
 	return "Contact not found";
