f81965175ad2a39d1e3f77981152a30c8523d449
Anners <anners@mailzorz.com>
2020-02-17 11:10:32 +0100

    fixade tester i UI test

--- Test/testfiles/UITest.java
+++ Test/testfiles/UITest.java
@@ -1,40 +1,43 @@
 package testfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import ui.UI;
+import ui.GUI;
+
 
 class UITest {
 
 	@Test
 	void UISearchTest() {
-		UI.cb.addContact("Lasse", "Schweinschteiger", "mail@mail", "03142227242");
-		UI.cb.run();
-		assertEquals(UI.cbs.searchContact("lasse"), "Lasse Schweinschteiger, mail@mail, 03142227242");
-		UI.cb.contacts.clear();
+		GUI.cb.addContact("Lasse", "Schweinschteiger", "mail@mail", "03142227242");
+		GUI.cb.runGUI();
+		assertEquals(GUI.cbs.searchContact("lasse"), "Lasse Schweinschteiger, mail@mail, 03142227242");
+		GUI.cb.contacts.clear();
 	}
 	
 	@Test
 	void UIAdd1Contact() {
-		UI.cb.contacts.clear();
-		UI.cb.addContact("Janne", "Klasson", "mail@mail", "031-283442442");
-		UI.cb.run();
-		assertEquals(UI.cb.contacts.size(), 1);
-		UI.cb.contacts.clear();
+		GUI.cb.contacts.clear();
+		GUI.cb.addContact("Janne", "Klasson", "mail@mail", "031-283442442");
+		GUI.cb.runGUI();
+		assertEquals(GUI.cb.contacts.size(), 1);
+		GUI.cb.contacts.clear();
 	}
 	
 	@Test
 	public void UIRemove3Contacts() {
-		UI.cb.contacts.clear();
-		UI.cb.addContact("Rolph", "Jenkins", "mail@mail", "031-2822242");
-		UI.cb.removeContact(2);
-		UI.cb.removeContact(1);
-		UI.cb.removeContact(0);
-		UI.cb.run();
-		assertEquals(UI.cb.contacts.size(), 0);
-		UI.cb.contacts.clear();
+		GUI.cb.contacts.clear();
+		GUI.cb.addContact("Rolph", "Jenkins", "mail@mail", "031-2822242");
+		GUI.cb.addContact("Janne", "Klasson", "mail@mail", "031-283442442");
+		GUI.cb.addContact("Lasse", "Schweinschteiger", "mail@mail", "03142227242");
+		GUI.cb.removeContact(2);
+		GUI.cb.removeContact(1);
+		GUI.cb.removeContact(0);
+		GUI.cb.runGUI();
+		assertEquals(GUI.cb.contacts.size(), 0);
+		GUI.cb.contacts.clear();
 	}
 
 }
