3e5a9f8c72fcd82e15361c058135bbd88075c3ac
Anners <anners@mailzorz.com>
2020-02-17 10:38:14 +0100

    la till ContactBookSearch tests

--- Test/testfiles/ContactBookSearchTest.java
+++ Test/testfiles/ContactBookSearchTest.java
@@ -4,11 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import contacts.ContactBookSearch;
+import ui.UI;
+
 class ContactBookSearchTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void SearchForMissingContactTest() {
+		ContactBookSearch search = new ContactBookSearch();
+		assertEquals(search.searchContact("Arne"), "Contact not found");
+	}	@Test
+	void SearchForExistingContactTest2() {
+		UI.cb.addContact("Arne", "Bjarnesson", "mail@mail", "031-28347242");
+		UI.cb.run();
+		assertEquals(UI.cbs.searchContact("arne"), "Arne Bjarnesson, mail@mail, 031-28347242");
+	}	@Test
+	void SearchForExistingContactTest3() {
+		UI.cb.addContact("Arne", "Bjarnesson", "mail@mail", "031-28347242");
+		UI.cb.addContact("Jonas", "Bengtsson", "mail@mail", "031-28347242");
+		UI.cb.run();
+		assertEquals(UI.cbs.searchContact("jonas"), "Jonas Bengtsson, mail@mail, 031-28347242");
 	}
 
 }
