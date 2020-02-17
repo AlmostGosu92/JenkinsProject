9b8c94ed8e59923029df773009c89c4945b14550
Anners <anners@mailzorz.com>
2020-02-17 10:53:54 +0100

    la till pom fil

--- Test/testfiles/UITest.java
+++ Test/testfiles/UITest.java
@@ -12,24 +12,29 @@ class UITest {
 	void UISearchTest() {
 		UI.cb.addContact("Lasse", "Schweinschteiger", "mail@mail", "03142227242");
 		UI.cb.run();
-		assertEquals(UI.cbs.searchContact("arne"), "Arne Bjarnesson, mail@mail, 031-28347242");
+		assertEquals(UI.cbs.searchContact("lasse"), "Lasse Schweinschteiger, mail@mail, 03142227242");
+		UI.cb.contacts.clear();
 	}
 	
 	@Test
 	void UIAdd1Contact() {
+		UI.cb.contacts.clear();
 		UI.cb.addContact("Janne", "Klasson", "mail@mail", "031-283442442");
 		UI.cb.run();
 		assertEquals(UI.cb.contacts.size(), 1);
+		UI.cb.contacts.clear();
 	}
 	
 	@Test
 	public void UIRemove3Contacts() {
+		UI.cb.contacts.clear();
 		UI.cb.addContact("Rolph", "Jenkins", "mail@mail", "031-2822242");
 		UI.cb.removeContact(2);
 		UI.cb.removeContact(1);
 		UI.cb.removeContact(0);
 		UI.cb.run();
 		assertEquals(UI.cb.contacts.size(), 0);
+		UI.cb.contacts.clear();
 	}
 
 }
